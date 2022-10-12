b166730c73b36ae06823a5c99e4191e26ccc901a
mnr <113099111+mooraboinanare@users.noreply.github.com>
2022-10-12 10:34:44 +0530

    added_Two_Files

---
+++ ThrowKeyword.java
@@ -0,0 +1,27 @@
+class ThrowKeyWord{
+
+ void run (int a){
+
+if (a==18){
+
+throw new ArithmeticException ("you are entered wrong pin");
+
+}
+else {
+System.out.println("your phone is opened");
+
+}
+
+
+}
+
+public static void main(String[] args){
+
+ThrowKeyWord mn = new ThrowKeyWord();
+mn.run(18);
+
+System.out.println("hii successfully completed this program");
+
+}
+
+}
