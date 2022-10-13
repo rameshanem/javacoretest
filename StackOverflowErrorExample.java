1d8ed9a7a361ec33d09f47631e75e9690531e6c6
mnr <113099111+mooraboinanare@users.noreply.github.com>
2022-10-13 09:07:42 +0530

    some thing did

---
+++ StackOverflowErrorExample.java
@@ -0,0 +1,12 @@
+public class StackOverflowErrorExample {
+    public void print(int myInt) {
+        System.out.println(myInt);
+        print(myInt);
+       
+    }
+
+    public static void main(String[] args) {
+        StackOverflowErrorExample soee = new StackOverflowErrorExample();
+        soee.print(1);
+    }
+}
